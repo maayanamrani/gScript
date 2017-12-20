085f41c26cda068c06abbd10d8cbbcaa9f5ec3d3
maayanamrani <33892512+maayanamrani@users.noreply.github.com>
2017-12-20 17:06:06 +0200

    Update README.groovy

--- README.groovy
+++ README.groovy
@@ -1,87 +1,121 @@
-artifactory('Maayan-ohad') {
-  localRepository('maven-local') {
-  description "Public description"
-  notes "Some internal notes"
-  includesPattern "**/*" // default
-  excludesPattern "" // default
-  repoLayoutRef "maven-2-default"
-  // xrayIndex false
-  // blockXrayUnscannedArtifacts false
-  // xrayMinimumBlockedSeverity "" // "Minor" | "Major" | "Critical"
-//  propertySets { 
-  
-  //propertySet('property_set_name') {
-    //singleSelect('property_name') {
-      //defaultValue "value_1"
-     // value "value_1"
-      //value "value_2"
-    //}
-    //multiSelect('property_name_multi') {
-     // defaultValue "value_1"
-      //efaultValue "value_2"
-      //value "value_1"
-      //value "value_2"
-    //}
-    //anyValue('another_property_name') {
-     // defaultValue "value_1"
-      //value "value_1"
-    //}
-  //}}
-  archiveBrowsingEnabled false
-  blackedOut false // default
-  packageType "maven" 
-  
-  // packageType "debian"
-  // debianTrivialLayout false
-  
-  // packageType "docker"
-  // dockerApiVersion "V2" // "V1"
-  // forceDockerAuthentication false
-  // maxUniqueTags 0 // optional
-  
-  // packageType "gradle"
-  // maxUniqueSnapshots 0 // default
-  // handleReleases true // default
-  // handleSnapshots true // default
-  // suppressPomConsistencyChecks false // default
-  // snapshotVersionBehavior "unique" // "non-unique" | "deployer"
-  // checksumPolicyType "client-checksums" // default | "server-generated-checksums"
-  
-  // packageType "ivy"
-  // maxUniqueSnapshots 0 // default
-  // handleReleases true // default
-  // handleSnapshots true // default
-  // suppressPomConsistencyChecks false // default
-  // snapshotVersionBehavior "unique" // "non-unique" | "deployer"
-  // checksumPolicyType "client-checksums" // default | "server-generated-checksums"
-  
-  // packageType "maven"
-  // maxUniqueSnapshots 0 // default
-  // handleReleases true // default
-  // handleSnapshots true // default
-  // suppressPomConsistencyChecks false // default
-  // snapshotVersionBehavior "unique" // "non-unique" | "deployer"
-  // checksumPolicyType "client-checksums" // default | "server-generated-checksums"
-  
-  // packageType "nuget"
-  // maxUniqueSnapshots 0 // default
-  // forceNugetAuthentication false // default
-  
-  // packageType "sbt"
-  // maxUniqueSnapshots 0 // default
-  // handleReleases true // default
-  // handleSnapshots true // default
-  // suppressPomConsistencyChecks false // default
-  // snapshotVersionBehavior "unique" // "non-unique" | "deployer"
-  // checksumPolicyType "client-checksums" // default | "server-generated-checksums"
-  
-  // packageType "yum"
-  // yumRootDepth 0
-  // groupFileNames "groups.xml"
-  // calculateYumMetadata false
-  
-}
-  
-  
+//Script to create a user, group, local repository and permissions.
+repokey = userInput (
+  type : "STRING",
+  name : "RepoKey",
+  description : "Please provide a Repository Key"
+)
 
+maturity = userInput (
+  type : "STRING",
+  name : "Maturity",
+  description : "Please provide a value for maturity (example - dev, stage or prod)"
+)
+
+packagetype = userInput (
+  name : "Package Type",
+  type : "PACKAGE_TYPE",
+  value : "",
+  description : "Please provide a value for package type"
+)
+
+location = userInput (
+  type : "STRING",
+  name : "Location",
+  description : "Please provide a value for location (example - local, remote)"
+)
+
+art = userInput (
+  type : "ARTIFACTORY",
+  name : "Instance",
+  description : "Please select the artifactory instance to run against"
+)
+
+userName = userInput (
+  type : "STRING",
+  name : "User Name",
+  description : "Please provide a username"
+)
+
+groupNames = userInput (
+  type : "STRING",
+  name : "Group Names",
+  description : "Please provide a group/s name (comma separated, if multiple groups need to be created)"
+)
+
+permissionName = userInput (
+  type : "STRING",
+  name : "Permission Name",
+  description : "Please provide a permission name"
+)
+
+permissions = userInput (
+  type : "STRING",
+  name : "Permissions",
+  description : "Please enter the permissions that need to be granted (manage,delete,deploy,annotate,read) comma separated, if multiple permissions need to be granted"
+)
+
+repoNames = userInput (
+  type : "STRING",
+  name : "Repository Names",
+  description : "Please provide a repo/s name (comma separated, if multiple repositories need to be granted permissions)"
+)
+//construct a namespace
+//team-technology-maturity-location
+def localName = repokey + "-" + packagetype + "-" + maturity + "-" + location
+
+// Choose the correct default layout based on the repository type
+def getRepoLayout(rtype) {
+  def specialtypes = ["bower", "gradle", "ivy", "npm", "nuget", "sbt", "vcs", "composer", "conan", "puppet"]
+  if (rtype == "maven") return "maven-2-default"
+  else if (rtype in specialtypes) return rtype + "-default"
+  else return "simple-default"
+}
+
+artifactory(art.name) {
+  localRepository(localName) {
+    description "Public description"
+    notes "Some internal notes"
+    archiveBrowsingEnabled false
+    packageType packagetype
+    repoLayoutRef getRepoLayout(packagetype)
+  }
+  security {
+    groups {
+      for(groupName in groupNames.split(",")) {
+        group(groupName) {
+          description ''
+          autoJoin false
+        }
+      }
+    }
+    users {
+      user(userName) {
+        email 'login_1@jfrog.com'
+        password 'password'
+        admin false
+        profileUpdatable false
+        internalPasswordDisabled false
+        groups (groupNames.split(",") as List)
+      }
+    }
+    permissions {
+      permission(permissionName) {
+        includesPattern '**'
+        excludesPattern ''
+        anyLocal false
+        anyRemote false
+        anyDistribution false
+        repositories (repoNames.split(",") as List)
+        users {
+          "$userName" (permissions.split(",") as List)
+        }
+        groups {
+          def myGroups = groupNames.split(",") as List
+          myGroups.each {
+            "$it" (permissions.split(",") as List)
+          }
+        }
+      }
+    }
+  }
 }
